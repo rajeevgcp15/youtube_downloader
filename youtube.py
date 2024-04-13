ffcaeb2f6c54bdcdc7e5d80296ab7f515b695526
rajeevgcp15 <106531460+rajeevgcp15@users.noreply.github.com>
2024-04-13 16:10:10 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,5 +1,18 @@
 import streamlit as st
 from pytube import YouTube
-YouTube('https://youtu.be/2lAe1cqCOXo').streams.first().download()
-yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
-st.download_button(label = "downloaded_file", data = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download())
+//YouTube('https://youtu.be/2lAe1cqCOXo').streams.first().download()
+//yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
+//st.download_button(label = "downloaded_file", data = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download())
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+
+link = st.input("Enter the YouTube video URL: ")
+if st.button("Click to Download File")
+    Download(link)
