4cd7f4ce1eef6dcc2b5bb99425c7ced88424f5f6
rajeevgcp15 <106531460+rajeevgcp15@users.noreply.github.com>
2024-04-13 15:58:47 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -2,4 +2,4 @@ import streamlit as st
 from pytube import YouTube
 YouTube('https://youtu.be/2lAe1cqCOXo').streams.first().download()
 yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
-st.download_button("download file", str(yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()))
+st.download_button("download file", yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download())
